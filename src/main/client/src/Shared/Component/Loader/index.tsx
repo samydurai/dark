6dc2524a913882faa8536e9dffa9f44d928c9280
raiden-x <raambalajid@gmail.com>
2020-06-20 21:48:32 +0530

    adding loader

---
+++ src/main/client/src/Shared/Component/Loader/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Loader1, Loader2, Loader3, Holder } from "./styles";
+
+export default function Loader() {
+  return (
+    <Holder>
+      <div>
+        <Loader1 />
+        <Loader2 />
+        <Loader3 />
+      </div>
+    </Holder>
+  );
+}
